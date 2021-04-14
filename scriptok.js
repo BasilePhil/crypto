5e7945d4a6442348671b5f8d4f4ef8b19c91aef0
Basile Philippon <basileb1@hotmail.fr>
2021-04-14 23:01:33 +0200

    premier jet

---
+++ scriptok.js
@@ -0,0 +1,18 @@
+alert('Bienvenue Basile');
+
+var a = prompt('Quel est le code ?','????');
+var b = false;
+
+if (a == 1234) { b = true;}
+
+else { b = false};
+
+if (b == true) {alert('Nous pouvons commencer')}
+
+else {alert('Retournez vous coucher')};
+
+const dogeamount = document.getElementById('dogeamount');
+const plus = document.getElementsByClassName('plus');
+const titre = document.getElementsByTagName('h1');
+
+alert(+dogeamount);
