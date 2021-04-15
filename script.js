1544c76ccde9091e9ee8309443f47678c1a401f3
Basile Philippon <basileb1@hotmail.fr>
2021-04-15 19:46:53 +0200

    changement au niveau des animations

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-var chargement = document.getElementById('chargement');
+const chargement = document.getElementById('chargement');
 
+chargement.addEventListener('click', function() {          // On écoute l'événement click
+    elt.innerHTML = "C'est cliqué !";               // On change le contenu de notre élément pour afficher "C'est cliqué !"
+});
 
